845482b3a4674d4468abd221fdf94cdf74ff4d57
Jeremy Wheaton <jlwheaton@hotmail.com>
2013-11-19 13:58:57 -0400

    login (not yet done)

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ comp3753
 ========
 
 PHP Bookstore database
+
+git guide:
+
+git add .
+git commit -m "a message"
+git push origin master
+
--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 <input type="submit">
 </form>
 
+<form method="post" action="login.php">
+	<input type="text" name="number">
+	<input type="text" name="password">
+<input type="submit">
+</form>
 </body>
 </html>
