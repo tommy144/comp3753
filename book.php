98d62d1c101d67bc426db607ee42b024a4218b33
root <root@tommy-ThinkPad-T430u.(none)>
2013-11-20 14:23:11 -0400

    good stuff

--- book.php
+++ book.php
@@ -13,17 +13,17 @@
 <p>
 <?php
 	session_start(); 
-	echo $_GET['input'];
+	echo $_GET['var'];
 	$link = mysql_connect('localhost', 'root', 'steeze')
     or die('Could not connect: ' . mysql_error());
 	mysql_select_db('bookstore') or die('Could not select database');
 	//$query = 'select * from student';
 	//$query = 'SELECT * FROM department WHERE department.Name="'.$_POST['input'].'"';
-	$query = 'SELECT Title
+	$query = 'SELECT ISBN, Title
 FROM (join_Book_Section JOIN book on join_Book_Section.Book_ISBN=book.ISBN)
 JOIN department
 ON department.Code=join_Book_Section.Dept_Code
-WHERE department.Name="'.$_POST['input'].'"';
+WHERE department.Name="'.$_GET['var'].'"';
 	//echo $query;
 	$out = mysql_query($query) or die('query error ' . mysql_error());
 
@@ -31,9 +31,14 @@ WHERE department.Name="'.$_POST['input'].'"';
 	while ($line = mysql_fetch_array($out, MYSQL_ASSOC)) 
 	{
     	echo "\t<tr>\n";
-    	foreach ($line as $col_value)
+    	//echo $line[1];
+		$i = 1;
+		foreach ($line as $col_value)
 		{
-       		echo "\t\t<td>$col_value</td>\n";
+       		
+			echo "\t\t<td> <a href=bookinfo.php?var=".$col_value.">".$col_value."</a></td>\n";
+			
+			$i++;
     	}
     	echo "\t</tr>\n";
 	}
